c6fcd8e28457fafeb41c30884c4e3f986af96c47
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-09-15 09:20:57 -0400

    Turn off debug mode.

--- server.py
+++ server.py
@@ -108,4 +108,4 @@ def main():
 if __name__ == '__main__':
     print('Go visit http://localhost:5000/geopaging')
     app.secret_key = 'asdfasdf123'
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
